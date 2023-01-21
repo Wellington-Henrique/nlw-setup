ce17f613d8c545bb84e7dcc0b71026012641c9fa
Wellington Henrique <wellingtonhlc@gmail.com>
2023-01-20 22:40:13 -0300

    Implementação de rotas summary e toggle no backend

--- server/prisma/routes.ts
+++ server/prisma/routes.ts
@@ -70,4 +70,78 @@ export async function appRoutes(app: FastifyInstance) {
             completedHabits
         }
     })
+
+    app.patch('/habits/:id/toggle', async (request) => {
+        const toggleHabitParams = z.object({
+          id: z.string().uuid()
+        })
+    
+        const { id } = toggleHabitParams.parse(request.params)
+    
+        const today = dayjs().startOf('day').toDate()
+    
+        let day = await prisma.day.findFirst({
+          where: {
+            date: today
+          }
+        })
+    
+        if(!day) {
+          day = await prisma.day.create({
+            data: {
+              date: today
+            }
+          })
+        }
+    
+        const dayHabit = await prisma.dayHabit.findUnique({
+          where: {
+            day_id_habit_id: {
+              day_id: day.id,
+              habit_id: id
+            }
+          }
+        })
+    
+        if(dayHabit) {
+          await prisma.dayHabit.delete({
+            where: {
+              id: dayHabit.id
+            }
+          })
+        } else {
+          await prisma.dayHabit.create({
+            data: {
+              day_id: day.id,
+              habit_id: id
+            }
+          })
+        }
+    })
+
+    app.get('/summary', async () => {
+        const summary = await prisma.$queryRaw`
+            SELECT 
+                T0.id, 
+                T0.date,
+                (
+                    SELECT 
+                        CAST(count(*) as float)
+                    FROM day_habits TB
+                    WHERE TB.day_id = T0.id
+                ) as completed,
+                (
+                    SELECT 
+                        CAST(count(*) as float)
+                    FROM habit_week_days TB0
+                    JOIN habits TB1 ON TB1.id = TB0.habit_id
+                    WHERE 
+                        TB0.week_day = CAST(strftime('%w', T0.date/1000.0, 'unixepoch') as int)
+                        AND TB1.created_at <= T0.date
+                ) as amount
+            FROM days T0
+        `;
+
+        return summary;
+    })
 }
