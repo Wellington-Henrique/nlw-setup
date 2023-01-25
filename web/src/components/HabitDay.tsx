464d81d7abd0ac8ea8a953967833168f95fbd7cf
Wellington Henrique <wellingtonhlc@gmail.com>
2023-01-24 23:58:52 -0300

    Implementação de estilizações e animações

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,32 +1,49 @@
 import * as Popover from '@radix-ui/react-popover';
+
 import clsx from 'clsx';
 import ProgressBar from './ProgressBar';
+import dayjs from 'dayjs';
+import { HabitsList } from './HabitList';
+import { useState } from 'react';
 
 interface HabitDaysProps {
-    amountCompleted: number,
-    amount: number
+    date: Date,
+    defaultCompleted?: number,
+    amount?: number
 }
 
-function HabitDay({amountCompleted, amount}: HabitDaysProps) {
-    const completedPercentage = Math.round(amountCompleted/amount*100)
+export function HabitDay({date, defaultCompleted=0, amount=0}: HabitDaysProps) {
+    const [completed, setCompleted] = useState(defaultCompleted);
+    const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100)  : 0
+
+    const dayAndMonth = dayjs(date).format('DD/MM');
+    const dayOfWeek = dayjs(date).format('dddd');
+
+    function handleCompletedChanged(completed: number) {
+        setCompleted(completed)
+    }
 
     return(
         <Popover.Root>
             <Popover.Trigger 
-                className={clsx("w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg", {
-                    'bg-violet-900 border-violet-800' : completedPercentage > 0 && completedPercentage < 20,
-                    'bg-violet-800 border-violet-700' : completedPercentage >= 20 && completedPercentage < 40,
-                    'bg-violet-700 border-violet-600' : completedPercentage >= 40 && completedPercentage < 60,
-                    'bg-violet-600 border-violet-500' : completedPercentage >= 60 && completedPercentage < 80,
-                    'bg-violet-500 border-violet-400' : completedPercentage >= 80
-                })}/>
+                className={clsx("w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background", {
+                'bg-zinc-900 border-zinc-800': completedPercentage === 0,
+                'bg-violet-900 border-violet-500': completedPercentage > 0 && completedPercentage < 20,
+                'bg-violet-800 border-violet-500': completedPercentage >= 20 && completedPercentage < 40,
+                'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
+                'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
+                'bg-violet-500 border-violet-400': completedPercentage >= 80,
+                })}
+            />
                         
             <Popover.Portal>
                 <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
-                    <span className='font-semibold text-zinc-400'>terça-feira</span>
-                    <span className='mt-1 font-extrabold leading-tight text-3xl'>17/01</span>
+                    <span className='font-semibold text-zinc-400'>{dayOfWeek}</span>
+                    <span className='mt-1 font-extrabold leading-tight text-3xl'>{dayAndMonth}</span>
                     
                     <ProgressBar progress={completedPercentage}/>
+                    
+                    <HabitsList date={date} onCompletedChanged={handleCompletedChanged}/>
 
                     <Popover.Arrow height={8} width={16} className='fill-zinc-900'/>    
                 </Popover.Content>
@@ -34,5 +51,3 @@ function HabitDay({amountCompleted, amount}: HabitDaysProps) {
         </Popover.Root>
     );
 }
-
-export default HabitDay;
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,33 +1,64 @@
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
 import { generateRangeFromYearBegnning } from "../utils/generate-dates-from-year-beginning";
-import HabitDay from "./HabitDay";
+import { HabitDay } from "./HabitDay";
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 const summaryDates = generateRangeFromYearBegnning();
 const minimumSummaryDatesSize = 18 * 7;
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
+type Summary = Array<{
+    id: string,
+    date: string,
+    amount: number,
+    completed: number
+}>
+
 function SummaryTable () {
+    const [summaries, setSummaries] = useState<Summary>([]);
+    
+    useEffect(() => {
+        api.get('summary').then(resp => {
+            setSummaries(resp.data);
+        });
+    }, []);
+
     return (
         <div className="w-full flex">
             <div className="grid grid-rows-7 grid-flow-row gap-3">
                 {weekDays.map((weekDay, i) => 
-                <div key={`${weekDay}- ${i}`} className="text-zinc-400 text-xl h-10 w-10 flex itmes-center justify-center font-bold">
+                <div key={`${weekDay}-${i}`} className="text-zinc-400 text-xl h-10 w-10 flex itmes-center justify-center font-bold">
                     {weekDay}
                 </div>)}
             </div>
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-                {summaryDates.map(date => 
+                {
+                summaries.length &&
+                summaryDates.map((date) => {
+                    const dayInSummary = summaries.find(day => {
+                    return dayjs(date).isSame(day.date, 'day')
+                })
+
+                return (
                 <HabitDay 
                     key={date.toString()}
-                    amount={5} 
-                    amountCompleted={Math.random() * 5} 
-                />)}
+                    date={date}
+                    amount={dayInSummary?.amount} 
+                    defaultCompleted={dayInSummary?.completed} 
+                />
+                )
+                })
+            }
 
-                {amountOfDaysToFill > 0 && 
+            {
+                amountOfDaysToFill > 0 && 
                 Array.from({length: amountOfDaysToFill}).map((_, i) => 
-                <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
-                )}
+                    <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
+                )
+            }
             </div>
         </div>
     );
